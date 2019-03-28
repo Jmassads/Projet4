4812754342a9331f388bacf54b98e06350a09c79
Julia Assad <juliasajah85@gmail.com>
2019-03-28 09:58:33 +0100

    finished the add method in AdminChapters controller and the adminchapters/add.php view

--- app/controllers/AdminChapters.php
+++ app/controllers/AdminChapters.php
@@ -41,9 +41,104 @@ class AdminChapters extends Controller
   $this->view('adminchapters/index', $data);
   }
 
-  public function add(){
-    $this->view('adminchapters/add');
+  /**
+  * add
+  * Create: création des chapitres
+  * @return void
+  */
+ public function add()
+ {
+
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   // Nous récupérons ici les données postées par le formulaire
+   // les champs textuels (text, select, textarea, ...) sont copiés dans le tableau superglobal $_POST ;
+   $data = [
+    'title' => $_POST['title'],
+    'body' => $_POST['body'],
+    'image' => $_FILES['myfile']['name'],
+
+    'title_err' => '',
+    'body_err' => '',
+    'image_err' => '',
+   ];
+
+   $currentDir = getcwd();
+   $uploadDirectory = "/uploads/";
+
+   // $errors = []; // Store all foreseen and unforseen errors here
+   $data['image_err'] = []; // Store all foreseen and unforseen errors here
+
+   $fileExtensions = ['jpeg', 'jpg', 'png']; // Get all the file extensions
+
+   // les informations concernant les champs de type file sont enregistrées dans le tableau superglobal $_FILES ;
+
+   $fileName = $_FILES['myfile']['name']; // Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
+   $fileSize = $_FILES['myfile']['size']; // La taille du fichier en octets.
+   $fileTmpName = $_FILES['myfile']['tmp_name']; // L'adresse vers le fichier uploadé dans le répertoire temporaire.
+   $fileType = $_FILES['myfile']['type'];
+   $tmp = explode('.', $fileName);
+   $fileExtension = strtolower(end($tmp));
+
+   $uploadPath = $currentDir . $uploadDirectory . basename($fileName);
+
+   // Validate data
+   if (empty($data['title'])) {
+    $data['title_err'] = 'Ce champ ne peut pas être vide';
+   }
+   if (empty($data['body'])) {
+    $data['body_err'] = 'Ce champ ne peut pas être vide';
+   }
+
+   if (!in_array($fileExtension, $fileExtensions)) {
+    $data['image_err'][] = "Les fichiers autorises sont: .jpg, .jpeg, .png";
+   }
+   if ($fileSize > 2000000) {
+    $data['image_err'][] = "Le fichier ne doit pas depasser les 2MB";
+   }
+
+   // Make sure there are no errors with the title and content
+   if (empty($data['title_err']) && empty($data['body_err']) && empty($data['image'])) {
+
+    // if image uploaded without errors, we add the chapter into the database
+    if ($this->chapterModel->addChapter($data)) {
+     //  die('SUCCESS');
+     flash('chapter_message', 'Chapitre ajouté sans image');
+     redirect('adminChapters');
+    } else {
+     die('Il y a eu une erreur');
+    }
+   } else if (empty($data['title_err']) && empty($data['body_err']) && empty($data['image_err']) && !empty($data['image'])) {
+
+    // Validated (no image errors)
+
+    $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
+
+    if ($didUpload) {
+     // if image uploaded without errors, we add the chapter into the database
+     if ($this->chapterModel->addChapterWithImage($data)) {
+      //  die('SUCCESS');
+      flash('chapter_message', 'Chapitre ajouté avec image');
+      redirect('adminChapters');
+     } else {
+      die('Il y a eu une erreur');
+     }
+    }
+
+   } else {
+    $this->view('adminChapters/add', $data);
+   }
+
+  } else {
+   $data = [
+    'title' => '',
+    'body' => '',
+    'image' => '',
+   ];
+   $this->view('adminchapters/add', $data);
   }
+
+ }
+
 
   public function show($id){
       echo ($id);
--- app/views/adminchapters/add.php
+++ app/views/adminchapters/add.php
@@ -1,1 +1,47 @@
-add chapter
+<?php require APPROOT . '/views/inc/adminHeader.php';?>
+
+<header id="admin-main-header" class="py-2 bg-secondary text-white">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1><i class="fas fa-pencil-alt"></i> Ajouter un chapitre</h1>
+      </div>
+    </div>
+  </div>
+</header>
+
+
+<div class="container py-2">
+  <div class="card card-body bg-light mt-5">
+    <form action="<?php echo URLROOT; ?>/adminChapters/add" method="post" enctype="multipart/form-data">
+      <div class="form-group">
+        <label for="title">Titre: <sup>*</sup></label>
+        <input type="text" name="title"
+          class="form-control form-control-lg <?php echo (!empty($data['title_err'])) ? 'is-invalid' : ''; ?>"
+          value="<?php echo $data['title']; ?>">
+        <span class="invalid-feedback">
+          <?php echo $data['title_err']; ?></span>
+      </div>
+      <div class="form-group">
+        <label for="body">Contenu: <sup>*</sup></label>
+        <textarea class="mytextarea" name="body"
+          class="form-control form-control-lg <?php echo (!empty($data['body_err'])) ? 'is-invalid' : ''; ?>"><?php echo $data['body']; ?></textarea>
+        <span class="invalid-feedback">
+          <?php echo $data['body_err']; ?></span>
+      </div>
+      <div class="form-group">
+        <label>Sélectionner une image:</label>
+        <input type="file" class="form-control-file" name="myfile">
+        <?php if (isset($data['image_err'])): ?>
+        <?php foreach ($data['image_err'] as $error): ?>
+        <span><?php echo $error . '</br>'; ?></span>
+        <?php endforeach;?>
+        <?php endif;?>
+        <div class="mt-4">
+          <input type="submit" class="btn btn-success" value="Publier">
+        </div>
+    </form>
+  </div>
+</div>
+
+<?php require APPROOT . '/views/inc/adminFooter.php';?>
