3e721d64eceac0948ecb7375cf99c25bf3713cd4
Julia Assad <juliasajah85@gmail.com>
2019-03-28 21:43:57 +0100

    added the userModel in the Home controller construct method to use it in index method and show the author's profile picture in homepage

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -13,17 +13,21 @@
       {
           $this->chapterModel = $this->model('AdminChapter');
           $this->bookModel = $this->model('AdminBook');
+          $this->userModel = $this->model('User');
       }
 
       public function index()
       {
+          $id = 1; // user with id 1 is Jean Forteroche
           $chapters = $this->chapterModel->getChapters();
           $books = $this->bookModel->getBooks();
+          $author = $this->userModel->getCurrentUser($id);
 
           $data = [
-       'chapters' => $chapters,
-       'books' => $books
-      ];
+             'chapters' => $chapters,
+             'books' => $books,
+             'author' => $author
+            ];
           $this->view('pages/index', $data);
       }
   }
