299cca0b7c1be4195d369f2d92ec42c4163a97ae
Julia Assad <juliasajah85@gmail.com>
2019-03-28 23:29:47 +0100

    added the chapterModel and bookModel into the constructor of the Admin controller and used the countChapters method from the chapterModel and countBooks method from the bookModel in the index method of the Admin controller, also updated the admin/index.php view

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -5,15 +5,30 @@
    */
   class Admin extends Controller
   {
+      // déclaration des propriétés
+      private $chapterModel;
+      private $bookModel;
+
       public function __construct()
       {
           if (!isset($_SESSION['user_id'])) {
               redirect('users/login');
           }
+
+          $this->chapterModel = $this->model('AdminChapter');
+          $this->bookModel = $this->model('AdminBook');
       }
 
       public function index()
       {
-          $this->view('admin/index');
+          $chaptersCount = $this->chapterModel->countChapters();
+          $booksCount = $this->bookModel->countBooks();
+
+          $data = [
+           'chaptersCount' => $chaptersCount,
+           'booksCount' => $booksCount,
+          ];
+
+          $this->view('admin/index', $data);
       }
   }
