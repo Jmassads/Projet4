3e04c4f38c013b355443fe184966c0670911f58f
Julia Assad <juliasajah85@gmail.com>
2019-04-01 22:49:32 +0200

    added inc/nav.php andrequired it in all front-end pages

--- app/views/pages/chapitres.php
+++ app/views/pages/chapitres.php
@@ -2,37 +2,7 @@
 
 <body class="page-template">
 
-  <!-- Pushy Menu -->
-  <nav class="pushy pushy-left">
-    <div class="pushy-content">
-      <ul>
-        <!-- Submenu -->
-        <li class="pushy-link"><a href="<?php echo URLROOT; ?>">Accueil</a></li>
-        <li class="pushy-submenu">
-          <button>Un Billet Simple pour l'Alaska</button>
-          <ul>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/chapitres">Tous les chapitres</a></li>
-            <?php foreach ($data['chapters'] as $chapter): ?>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/chapitres/<?php echo $chapter->id; ?>"><?php echo $chapter->title; ?></a>
-            </li>
-            <?php endforeach;?>
-          </ul>
-        </li>
-        <li class="pushy-submenu">
-          <button>Livres</button>
-          <ul>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/livres">Tous les livres</a></li>
-            <?php foreach ($data['books'] as $book): ?>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/livres/<?php echo $book->id; ?>"><?php echo $book->title; ?></a></li>
-            <?php endforeach;?>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-  <!-- Site Overlay -->
-  <div class="site-overlay"></div>
+  <?php require APPROOT . '/views/inc/nav.php';?>
 
   <div id="container">
     <div class="page-banner">
@@ -73,7 +43,7 @@
         </ul>
       </div>
     </section>
-    
+
   </div>
 
 
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,37 +2,7 @@
 
 <body class="front-page">
 
-  <!-- Pushy Menu -->
-  <nav class="pushy pushy-left">
-    <div class="pushy-content">
-      <ul>
-        <!-- Submenu -->
-        <li class="pushy-link"><a href="<?php echo URLROOT; ?>">Accueil</a></li>
-        <li class="pushy-submenu">
-          <button>Un Billet Simple pour l'Alaska</button>
-          <ul>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/chapitres">Tous les chapitres</a></li>
-            <?php foreach ($data['chapters'] as $chapter): ?>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/chapitres/<?php echo $chapter->id; ?>"><?php echo $chapter->title; ?></a>
-            </li>
-            <?php endforeach;?>
-          </ul>
-        </li>
-        <li class="pushy-submenu">
-          <button>Livres</button>
-          <ul>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/livres">Tous les livres</a></li>
-            <?php foreach ($data['books'] as $book): ?>
-            <li class="pushy-link"><a href="<?php echo URLROOT; ?>/livres/<?php echo $book->id; ?>"><?php echo $book->title; ?></a></li>
-            <?php endforeach;?>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
-  <!-- Site Overlay -->
-  <div class="site-overlay"></div>
+  <?php require APPROOT . '/views/inc/nav.php';?>
 
   <div id="container">
     <header class="d-flex flex-column justify-content-center align-items-center">
