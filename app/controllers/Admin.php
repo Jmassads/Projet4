1eed35c03e15e5696e81497237c9beadc40385ae
Julia Assad <juliasajah85@gmail.com>
2019-04-02 15:45:13 +0200

    instantiated the Comment model in the Admin controller and used the countComments method from the model to show how many comments there are on the admin view

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -8,6 +8,7 @@
       // déclaration des propriétés
       private $chapterModel;
       private $bookModel;
+      private $commentModel;
 
       public function __construct()
       {
@@ -17,16 +18,19 @@
 
           $this->chapterModel = $this->model('AdminChapter');
           $this->bookModel = $this->model('AdminBook');
+          $this->commentModel = $this->model('Comment');
       }
 
       public function index()
       {
           $chaptersCount = $this->chapterModel->countChapters();
           $booksCount = $this->bookModel->countBooks();
+          $commentsCount = $this->commentModel->countComments();
 
           $data = [
            'chaptersCount' => $chaptersCount,
            'booksCount' => $booksCount,
+           'commentsCount' => $commentsCount,
           ];
 
           $this->view('admin/index', $data);
--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,30 +1,36 @@
 <?php require APPROOT . '/views/inc/adminHeader.php';?>
 
 <header id="admin-main-header" class="py-2 bg-secondary text-white">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1><i class="fas fa-cog"></i> Accueil</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1><i class="fas fa-cog"></i> Accueil</h1>
+      </div>
     </div>
+  </div>
 </header>
 
 <div class="dashboard-items container py-5">
-    <a href="<?php echo URLROOT; ?>/adminChapters">
-        <div class="card card-body bg-light mb-4">
-            <p><i class="fas fa-pencil-alt"></i><strong>
-                    <?php echo $data['chaptersCount']; ?></strong> chapitres du livre 'Un Billet Simple Pour
-                L'Alaska' ont été publiés</p>
-        </div>
-    </a>
-    <a href="<?php echo URLROOT; ?>/adminBooks">
-        <div class="card card-body bg-light mb-4">
-            <p><i class="fas fa-book"></i><strong>
-                    <?php echo $data['booksCount']; ?></strong> livres ont été publiés</p>
-        </div>
-    </a>
-    
+  <a href="<?php echo URLROOT; ?>/adminChapters">
+    <div class="card card-body bg-light mb-4">
+      <p><i class="fas fa-pencil-alt"></i><strong>
+          <?php echo $data['chaptersCount']; ?></strong> chapitres du livre 'Un Billet Simple Pour
+        L'Alaska' ont été publiés</p>
+    </div>
+  </a>
+  <a href="<?php echo URLROOT; ?>/adminBooks">
+    <div class="card card-body bg-light mb-4">
+      <p><i class="fas fa-book"></i><strong>
+          <?php echo $data['booksCount']; ?></strong> livres ont été publiés</p>
+    </div>
+  </a>
+  <a href="<?php echo URLROOT; ?>/adminComments">
+    <div class="card card-body bg-light mb-4">
+      <p><i class="fas fa-comments"></i> Vous avez <strong>
+          <?php echo $data['commentsCount']; ?></strong> commentaires</p>
+    </div>
+  </a>
+
 </div>
 
 
